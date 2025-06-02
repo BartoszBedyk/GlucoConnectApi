26d96134800e677274f0377cbe1d2c22488ce6b2
BartoszBedyk <bartoszbedyk@gmail.com>
2025-06-02 16:05:12 +0200

    new report font Roboto UTF-8

--- app/src/main/kotlin/com/example/documentGenerator/DocumentService/PdfDocumentRenderer.kt
+++ app/src/main/kotlin/com/example/documentGenerator/DocumentService/PdfDocumentRenderer.kt
@@ -8,11 +8,13 @@ import infrastructure.UserService
 import io.ktor.server.util.*
 import io.ktor.util.*
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.net.URL
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.time.format.DateTimeFormatter
 import java.util.*
+
 
 class PdfDocumentRenderer
     (
@@ -32,8 +34,8 @@ class PdfDocumentRenderer
         val html = thymeleafService.render("glucose-report-template.html", mapOf(
             "glucose" to glucoseResults,
             "user" to user,
-            "startDate" to startDate.toLocalDateTime().toLocalDate().toString(),
-            "endDate" to endDate.toLocalDateTime().toLocalDate().toString(),
+            "startDate" to startDate,
+            "endDate" to endDate,
             "chartBase64" to chartBase64
         ))
 
@@ -41,6 +43,7 @@ class PdfDocumentRenderer
         PdfRendererBuilder()
             .useFastMode()
             .withHtmlContent(html, null)
+            .useFont(File("app/src/main/resources/assets/fonts/Roboto-Regular.ttf"), "Roboto")
             .toStream(outputStream)
             .run()
 
