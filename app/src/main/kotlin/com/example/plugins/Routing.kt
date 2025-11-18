f7db905c17eb0048cfc05119c33a2a649d6996b8
BartoszBedyk <bartoszbedyk@gmail.com>
2025-11-18 21:32:34 +0100

    Detekt fixes v 3

--- app/src/main/kotlin/com/example/plugins/Routing.kt
+++ app/src/main/kotlin/com/example/plugins/Routing.kt
@@ -39,7 +39,8 @@ import rest.userMedicationRoutes
 import rest.userRoutes
 import java.util.Date
 import javax.sql.DataSource
-@Suppress("MagicNumber")
+@Suppress("MagicNumber", "LongMethod")
+
 // detekt:disable LongMethod
 fun Application.configureRouting(dataSource: DataSource) {
     val dotenv = dotenv()
